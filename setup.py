bd3a4f6fc830523fbb7d678416ea73191cfd622b
atsukoba <atsuya_wgp4346@keio.jp>
2018-11-11 12:20:43 +0900

    update setup.py, README, add gitignore

--- setup.py
+++ setup.py
@@ -21,15 +21,17 @@ class ssdse:
         print("matplotlib lang set to Ja, using IPA-Gothic font.")
         
         self.ssdse = pd.read_csv(file_name, header=1)
-        
-        f_per_capita = lambda x: x / self.ssdse["人口総数"]
+
+        f_per_capita = lambda x: (x / self.ssdse["人口総数"]) * 100
         self.per_capita = self.ssdse.iloc[:, 3:].apply(f_per_capita)
-        self.per_capita = pd.concat([self.ssdse.iloc[:, :4],
+        self.per_capita = pd.concat([self.ssdse.iloc[:, :3],
                                      self.per_capita], axis=1)
         
         print("imported DataFrames from {}".format(file_name))
-        
+
         display(self.ssdse.head(5))
 
 
-
+if (__name__ == "__main__") or (__name__ == "setup"):
+    ssdse = ssdse()
+    
